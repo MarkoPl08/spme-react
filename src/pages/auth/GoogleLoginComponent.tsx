661ef675013c54f60d30f244f3720f4425cd38bd
marko <marko.pleslic@gmail.com>
2024-06-17 02:22:54 +0200

    rollback to default google button because custom one is not working

--- src/pages/auth/GoogleLoginComponent.tsx
+++ src/pages/auth/GoogleLoginComponent.tsx
@@ -1,10 +1,7 @@
-// src/components/GoogleLoginComponent.tsx
-
-import React, { useRef } from 'react';
+import React from 'react';
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import { User } from '../../types/auth';
 import { useNavigate } from 'react-router-dom';
-import CustomButton from '../../components/CustomButton.tsx';
 
 interface GoogleLoginComponentProps {
     setUser: (user: User | null) => void;
@@ -13,7 +10,6 @@ interface GoogleLoginComponentProps {
 
 const GoogleLoginComponent: React.FC<GoogleLoginComponentProps> = ({ setUser, user }) => {
     const navigate = useNavigate();
-    const googleButtonRef = useRef<HTMLDivElement>(null);
 
     const handleSuccess = (response: CredentialResponse) => {
         fetch('http://localhost:3001/api/auth/google', {
@@ -43,23 +39,10 @@ const GoogleLoginComponent: React.FC<GoogleLoginComponentProps> = ({ setUser, us
         console.log('OAuth error occurred');
     };
 
-    const handleCustomButtonClick = () => {
-        if (googleButtonRef.current) {
-            googleButtonRef.current.querySelector('button')?.click();
-        }
-    };
-
     return (
         <>
             {!user && (
-                <>
-                    <div ref={googleButtonRef} style={{ display: 'none' }}>
-                        <GoogleLogin onSuccess={handleSuccess} onError={errorMessage} />
-                    </div>
-                    <CustomButton onClick={handleCustomButtonClick} variant="contained">
-                        Login with Google
-                    </CustomButton>
-                </>
+                <GoogleLogin onSuccess={handleSuccess} onError={errorMessage} />
             )}
         </>
     );
